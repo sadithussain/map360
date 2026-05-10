2d2643b8b7cedd4c35765c5a7ab65e49e8064b7f
Sadit Hussain <wfrblx2244@gmail.com>
2026-05-10 17:22:20 -0400

    Created user model

---
+++ backend/app/models/user_model.py
@@ -0,0 +1,21 @@
+import uuid
+
+from datetime import datetime, timezone
+from sqlalchemy import Column, String, Integer, DateTime
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+from app.db.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(UUID(as_uuid = True), primary_key = True, default = uuid.uuid4, index = True)
+    username = Column(String, unique = True, index = True, nullable = False)
+    email = Column(String, unique = True, index = True, nullable = False)
+    hashed_password = Column(String, nullable = False)
+
+    experience_points = Column(Integer, default = 0, nullable = False)
+
+    created_at = Column(DateTime, default = lambda: datetime.now(timezone.utc))
+
+    
